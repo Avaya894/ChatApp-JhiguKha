f64ccc46588946a05bd13550d739e8eacc872d6f
Avaya894 <bajracharyaavaya52@gmail.com>
2023-10-23 19:03:09 +0545

    Final socket working with any user able to join the chat and communicate, UI Missing git add .git add .

--- server.js
+++ server.js
@@ -1,38 +1,35 @@
-// server file 
+// server file
 const express = require("express");
-const app = express()
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-users = {}
+users = {};
 
-// Connection event handler 
-io.on('connection', (socket)=>{
-   socket.on('new-user-joined', name=>{
-       users[socket.id] = name;
+// Connection event handler
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (name) => {
+    users[socket.id] = name;
     //    socket.broadcast.emit('user-joined', name);
-       socket.broadcast.emit('user-joined', name);
-   })
+    console.log(`New user joined ${name}`);
+    socket.broadcast.emit("user-joined", name);
+  });
 
-   socket.on('send', message => {
-       socket.broadcast.emit('receive', {message: message, user: users[socket.id]})
-   });
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", { message: message, user: users[socket.id] });
+  });
 
-   socket.on('disconnect', message=>{
-       socket.broadcast.emit('leave', {user: users[socket.id]})
-   })
-
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("leave", { user: users[socket.id] });
+  });
 });
-
-
 
 // Handle Routes
-app.get('/', (req, res)=> {
-    res.send('Welcome!');
+app.get("/", (req, res) => {
+  res.send("Welcome!");
 });
 
-// Start the server 
-http.listen(3000, ()=>{
-    console.log('Server is listening on port 3000');
+// Start the server
+http.listen(3000, () => {
+  console.log("Server is listening on port 3000");
 });
-
